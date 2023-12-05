157952726a010c77b1a682b41114f51582686943
NlodeL <nlodelop@myuax.com>
2023-12-05 11:57:20 +0100

    acabo de añadir el codigo de enviroment.h ayudado por visual studio

--- Parte2/lookup.cpp
+++ Parte2/lookup.cpp
@@ -2,6 +2,10 @@
 int Environment::lookup(const std::string& symbol) const {
     auto it = symbolTable.find(symbol);
     if (it != symbolTable.end()) {
-        // Devolver el valor asociado al símbolo
+
         return it->second;
+    } else {
+        std::cout << "Advertencia: El símbolo '" << symbol << "' no existe en el entorno." << std::endl;
+        return 0;
+    }
 }
