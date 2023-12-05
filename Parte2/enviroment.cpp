bbea3e9c8f0c8b16776c3be128add1991df76eec
NlodeL <nlodelop@myuax.com>
2023-12-05 11:13:39 +0100

    cambio el codigo para solucionar un erro en el

--- Parte2/enviroment.cpp
+++ Parte2/enviroment.cpp
@@ -4,26 +4,33 @@
 #include <string>
 #include <map>
 
-class enviroment {
-private:
-    std::map<std::string, int> symbolTable;
-};
-
+class Environment {
 public:
+    // Constructor
+    Environment() {
+        // Puedes realizar cualquier inicialización necesaria aquí
+    }
 
+    // Función para agregar un símbolo y su valor al entorno
     void addSymbol(const std::string& symbol, int value) {
         symbolTable[symbol] = value;
     }
 
+    // Función para obtener el valor de un símbolo del entorno
     int getSymbolValue(const std::string& symbol) const {
+        // Verificar si el símbolo está presente en la tabla
         auto it = symbolTable.find(symbol);
         if (it != symbolTable.end()) {
             // Devolver el valor asociado al símbolo
             return it->second;
         } else {
+            // Si el símbolo no está en la tabla, lanzar una excepción o manejar de alguna manera
+            // En este ejemplo, simplemente devolvemos un valor predeterminado (0)
             return 0;
         }
-
-
-}
     }
+
+private:
+    // Tabla de símbolos utilizando std::map
+    std::map<std::string, int> symbolTable;
+};
