13c3df8d343ca6830dce6a86a541f3640f5426d0
NlodeL <nlodelop@myuax.com>
2023-12-05 11:07:07 +0100

    comienzo a desarollar la clase enviroment apoyandome en github copilot

--- Parte2/enviroment.cpp
+++ Parte2/enviroment.cpp
@@ -4,3 +4,19 @@
 #include <string>
 #include <map>
 
+class enviroment {
+public:
+    enviroment() {
+        this->enviroment = std::map<std::string, std::string>();
+    }
+
+    void set(std::string key, std::string value) {
+        this->enviroment[key] = value;
+    }
+
+    std::string get(std::string key) {
+        return this->enviroment[key];
+    }
+
+
+};
