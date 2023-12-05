46ce0dbf6eeb25b4994926c1f953cbb96101ca6a
NlodeL <nlodelop@myuax.com>
2023-12-05 11:55:33 +0100

    continuo contruyendo el lookup

--- Parte2/lookup.cpp
+++ Parte2/lookup.cpp
@@ -1,2 +1,7 @@
 #include "Enviroment.h"
 int Environment::lookup(const std::string& symbol) const {
+    auto it = symbolTable.find(symbol);
+    if (it != symbolTable.end()) {
+        // Devolver el valor asociado al símbolo
+        return it->second;
+}
