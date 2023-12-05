1af73a7b5a6661f9d6cc106ee41b6319da1961f3
NlodeL <nlodelop@myuax.com>
2023-12-05 12:15:17 +0100

    Por falta de tiempo modifico minimamente la ayuda de visual studio para luego pegarl el código directo

--- Parte2/variant.h
+++ Parte2/variant.h
@@ -6,8 +6,30 @@
 #define EXAMEN_TEMA3_VARIANT_H
 
 
-class variant {
+class Variant {
+public:
+    Variant() : type(Type::None) {}
 
+    // Constructores para diferentes tipos de datos
+    Variant(int intValue) : intValue(intValue), type(Type::Int) {}
+    Variant(double doubleValue) : doubleValue(doubleValue), type(Type::Double) {}
+    Variant(const std::string& stringValue) : stringValue(stringValue), type(Type::String) {}
+
+    // Función para obtener el tipo actual
+    Type getType() const { return type; }
+
+    // Funciones para obtener los valores de los diferentes tipos
+    int getIntValue() const { return intValue; }
+    double getDoubleValue() const { return doubleValue; }
+    const std::string& getStringValue() const { return stringValue; }
+
+private:
+    enum class Type { None, Int, Double, String };
+
+    Type type;
+    int intValue;
+    double doubleValue;
+    std::string stringValue;
 };
 
 
